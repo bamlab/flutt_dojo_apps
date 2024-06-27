838a54cde77891b23f6d11ca54ea5cf36fbc672a
Paul <paulb@bam.tech>
2024-06-27 11:50:14 +0200

    dojo 13/06

--- flutter_app/lib/task_list/view/task_list_view.dart
+++ flutter_app/lib/task_list/view/task_list_view.dart
@@ -1,5 +1,7 @@
+import 'dart:ffi';
 import 'dart:ui';
 
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dojo_apps/task_list/widgets/task_list_header.dart';
 import 'package:flutter_dojo_apps/task_list/widgets/task_list_switcher.dart';
@@ -13,6 +15,7 @@ class TaskListView extends StatefulWidget {
 
 class _TaskListViewState extends State<TaskListView> {
   TodoState todoState = TodoState.todo;
+  List<TodoObject> todos = [];
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +25,7 @@ class _TaskListViewState extends State<TaskListView> {
           filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
           child: SafeArea(
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 const TaskListHeader(
                   projects: [
@@ -57,11 +61,62 @@ class _TaskListViewState extends State<TaskListView> {
                     },
                   ),
                 ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: todos.length,
+                    itemBuilder: (context, index) => Dismissible(
+                      key: Key(index.toString()),
+                      onDismissed: (_) => setState(() {
+                        // todos[index].setToDone();
+                        todos.removeAt(index);
+                      }),
+                      child: TodoCard(
+                        todo: todos[index],
+                      ),
+                    ),
+                  ),
+                ),
+                Dismissible(
+                  key: Key("add"),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          todos.add(TodoObject(name: "New task"));
+                        });
+                      },
+                      child: Text("Ajouter une tâche")),
+                )
               ],
             ),
           ),
         ),
       ],
     );
+  }
+}
+
+class TodoCard extends StatelessWidget {
+  final TodoObject todo;
+
+  const TodoCard({required this.todo});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.white,
+      title: Text(todo.name),
+      trailing: Icon(Icons.keyboard_arrow_down),
+    );
+  }
+}
+
+class TodoObject {
+  final String name;
+  bool done;
+
+  TodoObject({required this.name}) : done = false;
+
+  void setToDone() {
+    this.done = true;
   }
 }
